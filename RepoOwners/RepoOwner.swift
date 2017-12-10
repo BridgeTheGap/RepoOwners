91d6863ffb03af4c84b39a651a9ea7ece07ce8fe
Joshua Park <joshuaparkwm@gmail.com>
2017-12-10 19:06:51 +0900

    Add: RepoOwner

---
+++ RepoOwners/RepoOwner.swift
@@ -0,0 +1,16 @@
+//
+//  RepoOwner.swift
+//  RepoOwners
+//
+//  Created by Joshua Park on 10/12/2017.
+//  Copyright © 2017 Joshua Park. All rights reserved.
+//
+
+import Foundation
+
+struct RepoOwner {
+    
+    let avatarURL: String
+    let name: String
+    
+}
